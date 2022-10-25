673744a96b53df47d439e09bf7d5eec5a49a99c2
Davide Andrea Guastella <davide.guastella@icar.cnr.it>
2022-10-25 17:52:59 +0200

    Added java delegates for all alternative capabilities

---
+++ src/main/java/nettunit/handler/alternative_services/ensure_presence_of_representative_alt.java
@@ -0,0 +1,50 @@
+package nettunit.handler.alternative_services;
+
+import RabbitMQ.JixelEvent;
+import nettunit.JixelDomainInformation;
+import nettunit.NettunitService;
+import nettunit.SpringContext;
+import nettunit.rabbitMQ.ConsumerService.JixelRabbitMQConsumerService;
+import nettunit.rabbitMQ.ProducerService.MUSAProducerService;
+import org.flowable.engine.delegate.BpmnError;
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+import org.flowable.engine.impl.persistence.entity.ExecutionEntityImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+import static nettunit.NettunitService.JIXEL_EVENT_VAR_NAME;
+
+public class ensure_presence_of_representative_alt implements JavaDelegate {
+
+    private static Logger logger = LoggerFactory.getLogger(ensure_presence_of_representative_alt.class);
+
+    @Override
+    public void execute(DelegateExecution execution) {
+        JixelRabbitMQConsumerService jixelRabbitMQConsumerService = SpringContext.getBean(JixelRabbitMQConsumerService.class);
+        MUSAProducerService MUSAProducer = SpringContext.getBean(MUSAProducerService.class);
+        NettunitService nettunit = SpringContext.getBean(NettunitService.class);
+        if (nettunit.FailingTaskName.isPresent()) {
+            if (nettunit.FailingTaskName.get().equals(this.getClass().getName())) {
+                String taskName = ((ExecutionEntityImpl) execution).getActivityName();
+                String taskID = execution.getId();
+                nettunit.FailedTaskName = Optional.of(taskName);
+                nettunit.FailedTaskImplementation = Optional.of(this.getClass().getName());
+                throw new BpmnError("REQUIRE_ORCHESTRATION",this.getClass().getName());
+            }
+        }
+        logger.info("Executing capability ["+execution.getId()+"]: " + this.getClass().getSimpleName());
+
+
+        JixelEvent evt = (JixelEvent) execution.getVariable(JIXEL_EVENT_VAR_NAME);
+        String taskID = execution.getId();
+        //jixelRabbitMQConsumerService.save(evt, taskID);
+        //jixelRabbitMQConsumerService.save(evt, taskID);
+        MUSAProducer.addRecipient(evt, JixelDomainInformation.ASP);
+        MUSAProducer.addRecipient(evt, JixelDomainInformation.ARPA);
+
+        //throw new BpmnError("REQUIRE_ORCHESTRATION");
+    }
+}
---
+++ src/main/java/nettunit/handler/alternative_services/inform_technical_rescue_organisation_alert_alt.java
@@ -0,0 +1,54 @@
+package nettunit.handler.alternative_services;
+
+import RabbitMQ.JixelEvent;
+import nettunit.JixelDomainInformation;
+import nettunit.NettunitService;
+import nettunit.SpringContext;
+import nettunit.rabbitMQ.ConsumerService.JixelRabbitMQConsumerService;
+import nettunit.rabbitMQ.ProducerService.MUSAProducerService;
+import org.flowable.engine.delegate.BpmnError;
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+import org.flowable.engine.impl.persistence.entity.ExecutionEntityImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+import static nettunit.NettunitService.JIXEL_EVENT_VAR_NAME;
+
+public class inform_technical_rescue_organisation_alert_alt implements JavaDelegate {
+
+    /**
+     * Service that handle the messages that are consumed by Jixel. Please note that this is necessary only for testing
+     * purposes. Once the nettunit platform is deployed, this will not be used as this service will be available from
+     * IES solution.
+     */
+    private static Logger logger = LoggerFactory.getLogger(inform_technical_rescue_organisation_alert_alt.class);
+
+    @Override
+    public void execute(DelegateExecution execution) {
+        JixelRabbitMQConsumerService jixelRabbitMQConsumerService = SpringContext.getBean(JixelRabbitMQConsumerService.class);
+        MUSAProducerService MUSAProducer = SpringContext.getBean(MUSAProducerService.class);
+        NettunitService nettunit = SpringContext.getBean(NettunitService.class);
+        if (nettunit.FailingTaskName.isPresent()) {
+            if (nettunit.FailingTaskName.get().equals(this.getClass().getName())) {
+                String taskName = ((ExecutionEntityImpl) execution).getActivityName();
+                String taskID = execution.getId();
+                nettunit.FailedTaskName = Optional.of(taskName);
+                nettunit.FailedTaskImplementation = Optional.of(this.getClass().getName());
+                throw new BpmnError("REQUIRE_ORCHESTRATION", this.getClass().getName());
+            }
+        }
+        logger.info("Executing capability ["+execution.getId()+"]: " + this.getClass().getSimpleName());
+
+
+        JixelEvent evt = (JixelEvent) execution.getVariable(JIXEL_EVENT_VAR_NAME);
+        String taskID = execution.getId();
+        //jixelRabbitMQConsumerService.save(evt, taskID);
+        //jixelRabbitMQConsumerService.save(evt, taskID);
+        MUSAProducer.addRecipient(evt, JixelDomainInformation.ASP);
+        MUSAProducer.addRecipient(evt, JixelDomainInformation.ARPA);
+
+    }
+}
